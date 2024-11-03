0edd64491152caea95bc97e794cbdd5c0bce3f65
Atul Tiwari <tiwariatul9526@gmail.com>
2024-11-03 21:52:27 +0530

    props typescript done

---
+++ practice-prj/src/App.tsx
@@ -0,0 +1,10 @@
+import { User } from "./components/User";
+
+const App = () => {
+  return (
+    <div>
+      <User name="Darling" age={20} isStudent={true} />
+    </div>
+  );
+};
+export default App;
