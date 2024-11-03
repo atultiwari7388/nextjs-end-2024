4269b686f2449e340fa6cc300e27bfb1c554e5f3
Atul Tiwari <tiwariatul9526@gmail.com>
2024-11-03 21:53:01 +0530

    destructuring done

--- practice-prj/src/components/User.tsx
+++ practice-prj/src/components/User.tsx
@@ -1,13 +1,17 @@
-export const User = (props: {
+export const User = ({
+  name,
+  age,
+  isStudent,
+}: {
   name: string;
   age: number;
   isStudent: boolean;
 }) => {
   return (
     <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.isStudent}</h1>
+      <h1>{name}</h1>
+      <h1>{age}</h1>
+      <h1>{isStudent}</h1>
     </div>
   );
 };
