d37cf5c50aa2c4cb9535eca0db3593b032adbd63
Atul Tiwari <tiwariatul9526@gmail.com>
2024-11-04 14:16:41 +0530

    radix theme done

---
+++ radix-theme-practice/src/App.jsx
@@ -0,0 +1,10 @@
+import { Flex, Text, Button } from "@radix-ui/themes";
+
+export const App = () => {
+  return (
+    <Flex direction="column" gap="2">
+      <Text>Hello from Radix Themes :)</Text>
+      <Button>Let&apos;s go</Button>
+    </Flex>
+  );
+};
