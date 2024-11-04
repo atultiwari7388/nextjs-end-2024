77d3f103d9d133df572255fffe085f74937d54ed
Atul Tiwari <tiwariatul9526@gmail.com>
2024-11-04 13:59:52 +0530

    daisy ui done

--- practice-prj/src/components/Hero.tsx
+++ practice-prj/src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export const Hero = () => {
   return (
     <>
-      <div className="bg-white">
+      <div className="dark">
         <header className="absolute inset-x-0 top-0 z-50">
           <nav
             className="flex items-center justify-between p-6 lg:px-8"
@@ -41,22 +41,24 @@ export const Hero = () => {
               </button>
             </div>
             <div className="hidden lg:flex lg:gap-x-12">
-              <a href="#" className="text-sm/6 font-semibold text-gray-900">
+              <a href="#" className="text-sm/6 font-semibold text-white">
                 Product
               </a>
-              <a href="#" className="text-sm/6 font-semibold text-gray-900">
+              <a href="#" className="text-sm/6 font-semibold text-white">
                 Features
               </a>
-              <a href="#" className="text-sm/6 font-semibold text-gray-900">
+              <a href="#" className="text-sm/6 font-semibold text-white">
                 Marketplace
               </a>
-              <a href="#" className="text-sm/6 font-semibold text-gray-900">
+              <a href="#" className="text-sm/6 font-semibold text-white">
                 Company
               </a>
             </div>
             <div className="hidden lg:flex lg:flex-1 lg:justify-end">
               <a href="#" className="text-sm/6 font-semibold text-gray-900">
-                Log in <span aria-hidden="true">&rarr;</span>
+                <button className="btn btn-outline btn-primary text-white">
+                  Login <span aria-hidden="true">&rarr;</span>
+                </button>{" "}
               </a>
             </div>
           </nav>
