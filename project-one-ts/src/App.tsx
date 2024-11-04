0329da5891aef76a1361db8b2fc04645aa3b4e95
Atul Tiwari <tiwariatul9526@gmail.com>
2024-11-05 00:30:59 +0530

    project-one-done

---
+++ project-one-ts/src/App.tsx
@@ -0,0 +1,12 @@
+import { Profile } from "./components/Profile";
+import Sidebar from "./components/Sidebar";
+
+const App = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Profile />
+    </div>
+  );
+};
+export default App;
---
+++ project-one-ts/src/components/About.tsx
@@ -0,0 +1,31 @@
+const About = () => {
+  return (
+    <section className="bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-4xl mx-auto">
+        <h2 className="text-3xl font-extrabold text-gray-900 mb-6">
+          About Dimple
+        </h2>
+        <p className="text-lg text-gray-700">
+          Dimple is a gifted software engineer with a keen eye for crafting
+          seamless user experiences. With a rich background in various
+          technologies, frameworks, and languages—spanning React, Angular,
+          Python, Golang, and beyond—her expertise shines across a wide range of
+          projects. Dimple’s commitment to excellence and adaptability has led
+          her to develop impressive applications and solutions for clients
+          globally, each reflecting her mastery in her field.
+        </p>
+        <p className="mt-4 text-lg text-gray-700">
+          What truly sets Dimple apart is her passion for User Interface Design.
+          This fascination has driven her to continually refine her skills,
+          taking every opportunity to expand her knowledge through online
+          courses, workshops, and hands-on experience. Her dedication and
+          relentless curiosity have helped her become a standout UI/UX Designer,
+          known for her ability to bring creative, functional, and beautiful
+          designs to life.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
